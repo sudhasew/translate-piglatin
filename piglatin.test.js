e07b63f7423d459df8c40a701d781ee5161cb80a
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-01-04 15:18:05 -0500

    added some extra tests

--- piglatin.test.js
+++ piglatin.test.js
@@ -1,8 +1,43 @@
+//TRANSLATING BOTH THE VOWELS AND CONSONANTS TO PIG LATIN.
+
 const translate = require("./piglatin");
-test("sudha should produce udhasay", () => {
-  expect(translate("SUDHA")).toEqual("udhasay");
+
+//Words starting with consonants.
+
+test("banana should produce ananabay", () => {
+  expect(translate("banana")).toEqual("ananabay");
 });
+
+test("giraffe should produce iraffegay", () => {
+  expect(translate("giraffe")).toEqual("iraffegay");
+});
+
+test("sunshine should produce unshinesay", () => {
+  expect(translate("SUNSHINE")).toEqual("unshinesay");
+});
+
+test("sun should produce unsay", () => {
+  expect(translate("sun")).toEqual("unsay");
+});
+
+//Words starting with vowels.
 
 test("apple should produce appleway", () => {
   expect(translate("apple")).toEqual("appleway");
+});
+
+test("else should produce elseway", () => {
+  expect(translate("else")).toEqual("elseway");
+});
+
+test("indigo should produce indigoway", () => {
+  expect(translate("INDIGO")).toEqual("indigoway");
+});
+
+test("orange should produce orangeway", () => {
+  expect(translate("orange")).toEqual("orangeway");
+});
+
+test("umbrella should produce umbrellaway", () => {
+  expect(translate("umbrella")).toEqual("umbrellaway");
 });
