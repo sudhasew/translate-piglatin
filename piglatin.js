5b870c679f884b155d13ec5dd2bf85f5c8bc03e8
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-01-05 10:54:43 -0500

    modified js

--- piglatin.js
+++ piglatin.js
@@ -1,19 +1,33 @@
-function translate(str) {
-  str = str.toLowerCase();
-  var n = str.search(/[aeiuo]/);
-  switch (n) {
-    case 0:
-      str = str + "way";
-      break;
-    case -1:
-      str = str + "ay";
-      break;
-    default:
-      //str= str.substr(n)+str.substr(0,n)+"ay";
-      str = str.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
-      break;
-  }
-  return str;
-}
+// function translate(str) {
+//   str = str.toLowerCase();
+//   var n = str.search(/[aeiuo]/);
+//   switch (n) {
+//     case 0:
+//       str = str + "way";
+//       break;
+//     case -1:
+//       str = str + "ay";
+//       break;
+//     default:
+//       //str= str.substr(n)+str.substr(0,n)+"ay";
+//       str = str.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+//       break;
+//   }
+//   return str;
+// }
 
-module.exports = translate;
+// module.exports = translate;
+
+// ----------------------- OR ----------------------
+const functions = {
+  // Arrow function
+  vowel: function translate(str) {
+    str = str.toLowerCase();
+    return str.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1way");
+  },
+  consonant: function translate(str) {
+    str = str.toLowerCase();
+    return str.replace(/\b(\w)(\w+)\b/g, "$2$1ay");
+  },
+};
+module.exports = functions;
--- piglatin.test.js
+++ piglatin.test.js
@@ -1,43 +1,43 @@
 //TRANSLATING BOTH THE VOWELS AND CONSONANTS TO PIG LATIN.
 
-const translate = require("./piglatin");
+const functions = require("./piglatin");
 
 //Words starting with consonants.
 
 test("banana should produce ananabay", () => {
-  expect(translate("banana")).toEqual("ananabay");
+  expect(functions.consonant("banana")).toEqual("ananabay");
 });
 
 test("giraffe should produce iraffegay", () => {
-  expect(translate("giraffe")).toEqual("iraffegay");
+  expect(functions.consonant("giraffe")).toEqual("iraffegay");
 });
 
 test("sunshine should produce unshinesay", () => {
-  expect(translate("SUNSHINE")).toEqual("unshinesay");
+  expect(functions.consonant("SUNSHINE")).toEqual("unshinesay");
 });
 
 test("sun should produce unsay", () => {
-  expect(translate("sun")).toEqual("unsay");
+  expect(functions.consonant("sun")).toEqual("unsay");
 });
 
 //Words starting with vowels.
 
 test("apple should produce appleway", () => {
-  expect(translate("apple")).toEqual("appleway");
+  expect(functions.vowel("apple")).toEqual("appleway");
 });
 
 test("else should produce elseway", () => {
-  expect(translate("else")).toEqual("elseway");
+  expect(functions.vowel("else")).toEqual("elseway");
 });
 
 test("indigo should produce indigoway", () => {
-  expect(translate("INDIGO")).toEqual("indigoway");
+  expect(functions.vowel("INDIGO")).toEqual("indigoway");
 });
 
 test("orange should produce orangeway", () => {
-  expect(translate("orange")).toEqual("orangeway");
+  expect(functions.vowel("orange")).toEqual("orangeway");
 });
 
 test("umbrella should produce umbrellaway", () => {
-  expect(translate("umbrella")).toEqual("umbrellaway");
+  expect(functions.vowel("umbrella")).toEqual("umbrellaway");
 });
